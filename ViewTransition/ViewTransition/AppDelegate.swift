3847b6df040bb58311d04253acacf271957312c9
sircapel26 <edwincapel26@gmail.com>
2015-10-15 17:46:22 -0700

    added base ios code

---
+++ ViewTransition/ViewTransition/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ViewTransition
+//
+//  Created by Edwin Capel on 10/15/15.
+//  Copyright © 2015 Edwin Capel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
